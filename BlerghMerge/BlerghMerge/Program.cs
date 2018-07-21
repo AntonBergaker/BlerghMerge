6418d20753c343b352c698f6adbc82977e75f473
Anton <noten.b@hotmail.com>
2018-07-21 21:14:31 +0200

    JS and CSS and Blergh Ignore

--- BlerghMerge/BlerghMerge/Program.cs
+++ BlerghMerge/BlerghMerge/Program.cs
@@ -47,8 +47,66 @@ namespace BlerghMerge
             //Copy all files
             DirectoryCopy(sourcePath, targetPath, true);
 
+            //Do the preprocessing
+            PreProcessFiles(targetPath);
+
             //Move through all files applying our thingies
             UpdateFiles(targetPath);
+        }
+
+
+        private static void PreProcessFiles(string directoryPath)
+        {
+            DirectoryInfo dir = new DirectoryInfo(directoryPath);
+
+            // Get the files in the directory and run the update on them
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string[] possibleFiles = new string[] { "html", "htm", "js", "css", "txt", "json"};
+                string ext = file.Extension.Substring(1);
+                if (possibleFiles.Contains(ext))
+                {
+                    PreProcessFile(file.FullName);
+                }
+            }
+
+            // Recursively call the folders
+            foreach (DirectoryInfo subdir in dir.GetDirectories())
+            {
+                PreProcessFiles(subdir.FullName);
+            }
+        }
+
+        private static void PreProcessFile(string filePath)
+        {
+            List<string> content = File.ReadAllLines(filePath).ToList();
+            bool insideIgnore = false;
+            int ignoreStart = -1;
+
+            for (int i=0;i<content.Count;i++)
+            {
+                string line = content[i].ToLower();
+                if (!insideIgnore)
+                {
+                    if (line.Contains("//") && !line.Contains("end")
+                    && (line.Contains("blergh ignore") || line.Contains("blergh! ignore")))
+                    {
+                        insideIgnore = true;
+                        ignoreStart = i;
+                    }
+                } else
+                {
+                    if (line.Contains("//") && line.Contains("end")
+                    && (line.Contains("blergh ignore") || line.Contains("blergh! ignore")))
+                    {
+                        insideIgnore = false;
+                        content.RemoveRange(ignoreStart, i-ignoreStart+1);
+                        i = ignoreStart;
+                    }
+                }
+            }
+
+            File.WriteAllLines(filePath, content);
         }
 
         private static void UpdateFiles(string directoryPath)
@@ -97,11 +155,27 @@ namespace BlerghMerge
                         XmlDocument doc = new XmlDocument();
                         doc.LoadXml(line);
                         XmlNode newNode = doc.DocumentElement;
+                        string path = newNode.Attributes["href"].Value;
+                        path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(filePath), path));
+                        string[] lines = File.ReadAllLines(path);
+                        content.Insert(i++, "<style>");
+                        content.InsertRange(i, lines);
+                        i += lines.Length;
+                        content.Insert(i, "</style>");
                     }
                     // A script file
                     else if (line.Contains("<script"))
                     {
-
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(line);
+                        XmlNode newNode = doc.DocumentElement;
+                        string path = newNode.Attributes["src"].Value;
+                        path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(filePath), path));
+                        string[] lines = File.ReadAllLines(path);
+                        content.Insert(i++, "<script>");
+                        content.InsertRange(i, lines);
+                        i += lines.Length;
+                        content.Insert(i, "</script>");
                     }
                     // Or a html file
                     else if (line.Contains("class = \"imported") || line.Contains("class=\"imported"))
@@ -117,6 +191,15 @@ namespace BlerghMerge
             }
 
             File.WriteAllLines(filePath, content);
+        }
+
+        private static void IndentStrings(string[] strings, int indention, char indentChar = '\t')
+        {
+            int len = strings.Length;
+            for (int i = 0; i<len; i++)
+            {
+                strings[i] = new string(indentChar, indention) + strings[i];
+            }
         }
 
         /// <summary>
