8a931620b2d5f9fda021a50e2ccc4abf4e28d1b1
Anton <noten.b@hotmail.com>
2019-01-05 22:16:00 +0100

    Moved to different files

---
+++ BlerghMerge/BlerghMerge/CodeFile.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BlerghMerge {
+    class CodeFile {
+        protected int indentation;
+        protected char indentationChar;
+        protected string path;
+        
+
+        public CodeFile(string source, string documentPath) {
+            (indentation, indentationChar) = GetIndentionData(source);
+        }
+
+        public virtual List<string> ExportLines() {
+
+            string[] lines = File.ReadAllLines(path);
+            Indent(lines, indentation + 1, indentationChar);
+
+            return new List<string>(lines);
+        
+        }
+
+        public virtual string WrapContent(List<string> content) {
+            throw new NotImplementedException();
+        }
+
+        protected void Indent(string[] content, int indentation, char indentChar) {
+            for (int i = 0; i < content.Length; i++) {
+                content[i] = new string(indentChar, indentation) + content[i];
+            }
+        }
+
+        protected void Indent(List<string> content, int indentation, char indentChar) {
+            for (int i=0;i<content.Count;i++) {
+                content[i] = new string(indentChar, indentation) + content[i];
+            }
+        }
+
+        protected static XmlNode ReadXmlNode(string line) {
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(line);
+            return doc.DocumentElement;
+        }
+
+        protected static (int indentation, char indentationChar) GetIndentionData(string input) {
+            if (input.Length == 0) {
+                return (0, '\t');
+            }
+
+            char indentationChar = input.First();
+            int indentation = 0;
+            if (indentationChar == '\t' || indentationChar == ' ') {
+                indentation = CountRepeatChars(input);
+            }
+
+            return (indentation, indentationChar);
+        }
+
+        private static int CountRepeatChars(string str) {
+            if (str.Length == 0) {
+                return 0;
+            }
+            char firstChar = str.First();
+            int count = 1;
+            for (; count < str.Length; count++) {
+                if (str[count] != firstChar) {
+                    break;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ BlerghMerge/BlerghMerge/CodeFileCSS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BlerghMerge {
+    class CodeFileCSS : CodeFile {
+
+        public CodeFileCSS(string source, string sourcePath) : base(source, sourcePath) {
+            XmlNode newNode = ReadXmlNode(source);
+            path = newNode.Attributes["src"].Value;
+            path = Path.GetFullPath(Path.Combine(Path.GetDirectoryName(sourcePath), path));
+        }
+
+        public override string WrapContent(List<string> content) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(new string(indentationChar, indentation) + "<style>\n");
+            foreach(string s in content) {
+                sb.Append(s);
+            }
+            sb.Append(new string(indentationChar, indentation) + "</style>");
+
+            return sb.ToString();
+        }
+    }
+}
